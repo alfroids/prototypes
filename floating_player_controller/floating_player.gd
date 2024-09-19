0b3e1d8633588cafb0cb0dbfe1deefd06c751f47
Alfredo Reis <alf.reismarin@gmail.com>
2024-09-19 19:25:34 -0300

    Add animations to the FloatingPlayer's scene

--- floating_player_controller/floating_player.gd
+++ floating_player_controller/floating_player.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @export var acceleration: float = 600.0
 @export var drag: float = 250.0
 
+@onready var sprite_2d: Sprite2D = $Sprite2D as Sprite2D
+
 
 func _physics_process(delta: float) -> void:
 	var input: Vector2 = Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
@@ -15,4 +17,11 @@ func _physics_process(delta: float) -> void:
 
 	if velocity:
 		velocity = velocity.move_toward(Vector2.ZERO, delta * drag)
+
+	if not velocity:
+		sprite_2d.frame_coords = Vector2(0, 0)
+	else:
+		var x: int = 1 if velocity.x > move_speed / 4 else 2 if velocity.x < -move_speed / 4 else 0
+		var y: int = 1 if velocity.y < -move_speed / 4 else 2 if velocity.y > move_speed / 4 else 0
+		sprite_2d.frame_coords = Vector2(x, y)
 		move_and_slide()
